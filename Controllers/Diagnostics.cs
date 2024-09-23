e5860b3241c203014e6b1b5d74d0ee3cccc8182f
Alexander Damov <alexsander.damov@yahoo.com>
2024-09-23 14:59:25 +0300

    Add diagnostics controller to get configs

---
+++ Controllers/Diagnostics.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DiagnosticsController(IConfiguration config)
+{
+    [HttpGet]
+    public string GetConfigs()
+    {
+        var t = config.AsEnumerable().ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        return JsonSerializer.Serialize(t);
+    }
+}
